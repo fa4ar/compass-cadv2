78cf41dafa91c31346349103addf273c228846b1
fa4ar <evreone8@gmail.com>
2026-04-10 20:31:47 +0300

    Remove prisma.config.ts to use environment variables

--- backend/prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-    schema: "prisma/schema.prisma",
-    datasource: {
-        url: "postgresql://compasscaduser:TpWV6Ardlk5eBhzqQc@localhost:5432/cadnew?schema=public&sslmode=disable",
-    },
-});
